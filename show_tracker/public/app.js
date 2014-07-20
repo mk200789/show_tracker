2e548a2bd75d8f368d771899cf6942cfc4db9125
WANKIM MOK <cafe.mui@gmail.com>
2014-07-19 23:32:27 -0400

    included angularjs

---
+++ show_tracker/public/app.js
@@ -0,0 +1,5 @@
+angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
+  .config('$locationProvider', function($locationProvider) {
+  	$locationProvider.html5Mode(true);
+
+  });
