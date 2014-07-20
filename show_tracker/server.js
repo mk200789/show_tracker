f2beeacb218c62cf8808d810b7122c85971846e7
WANKIM MOK <cafe.mui@gmail.com>
2014-07-20 02:26:56 -0400

    included mongoose and the mongoose schema for this project

--- show_tracker/server.js
+++ show_tracker/server.js
@@ -1,3 +1,29 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+var showSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  airsDayOfWeek: String,
+  airsTime: String,
+  firstAired: Date,
+  genre: [String],
+  network: String,
+  overview: String,
+  rating: Number,
+  ratingCount: Number,
+  status: String,
+  poster: String,
+  subscribers: [{
+    type: mongoose.Schema.Types.ObjectId, ref: 'User'
+  }],
+  episodes: [{
+      season: Number,
+      episodeNumber: Number,
+      episodeName: String,
+      firstAired: Date,
+      overview: String
+  }]
+});
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
